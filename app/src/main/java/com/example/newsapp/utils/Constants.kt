e857130abc6d29299e78e4442e40a9370d977725
Tashpolotov <Aza662485@gmail.com>
2023-04-27 22:55:34 +0600

    lesson-3

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "11177f9413794e8682645adfe05606a5"
+    }
+}
